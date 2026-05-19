81eeaf07cae7f0b8c9ff3fb4fabac8ea287d9d5d
vtv44 <vtvminecraftserver@gmail.com>
2026-05-19 20:58:50 +0900

    a

--- src/form/Forms.ts
+++
@@ -1,9 +0,0 @@
-import { GameStartForm } from './forms/GameStartForm';
-import { LoreForm } from './forms/LoreForm';
-import { TeleportForm } from './forms/TeleportForm';
-
-export class Forms {
-    static TELEPORT = new TeleportForm();
-    static LORE = new LoreForm();
-    static GAME_START = new GameStartForm();
-}
--- src/game/item/ItemWearable.ts
+++
@@ -1,3 +0,0 @@
-export interface ItemWearable {
-    isFullEquipment(): boolean;
-}
