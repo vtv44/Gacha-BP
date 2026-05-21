5a1ac514e37d1092131ab3cefe220168532bc210
vtv44 <vtvminecraftserver@gmail.com>
2026-05-22 00:19:03 +0900

    giant

--- scripts/skill/skillRegister.js
+++ scripts/skill/skillRegister.js
@@ -1,4 +1,6 @@
 import { skillManager } from "./skillManager";
 import { redMagicSkill } from "./spell/redMagicSkill";
+import { giantSwordSkill } from "./weapon/giant_sword";
 
 skillManager.register(new redMagicSkill())
+skillManager.register(new giantSwordSkill())
---
+++ scripts/skill/weapon/giant_sword.js
@@ -0,0 +1,78 @@
+import { world, system } from "@minecraft/server";
+import { skillBase } from "../skillBase";
+
+export class giantSwordSkill extends skillBase {
+    constructor() {
+        super();
+        this.id = "§6ジャイアントソード";
+        this.cooldown = 20 * 20;
+    }
+
+    execute(player) {
+        const dimension = player.dimension;
+        const teamObjective = world.scoreboard.getObjective("team");
+        const teamScore = teamObjective ? teamObjective.getScore(player.name) ?? -1 : -1;
+
+        const viewDir = player.getViewDirection();
+        const pos = player.location;
+
+        const spawnPos = {
+            x: pos.x + viewDir.x * 8,
+            y: pos.y + viewDir.y * 8 + 20,
+            z: pos.z + viewDir.z * 8
+        };
+
+        const sword = dimension.spawnEntity("gacha:giant_ironsword", spawnPos);
+
+        sword.applyKnockback(0, 0, 0, -3);
+
+        let prevY = spawnPos.y;
+        let tickCount = 0;
+        const maxTicks = 200;
+
+        const checkId = system.runInterval(() => {
+            tickCount++;
+
+            if (!sword.isValid() || tickCount > maxTicks) {
+                system.clearRun(checkId);
+                return;
+            }
+
+            const currentY = sword.location.y;
+
+            if (tickCount > 5 && Math.abs(currentY - prevY) < 0.01) {
+                system.clearRun(checkId);
+
+                const landPos = sword.location;
+
+                dimension.runCommand(`particle minecraft:huge_explosion_emitter ${landPos.x} ${landPos.y} ${landPos.z}`)
+                dimension.runCommand(`playsound random.explode @a ${landPos.x} ${landPos.y} ${landPos.z}`)
+
+
+                const targets = dimension.getEntities({
+                    location: landPos,
+                    maxDistance: 6,
+                    type: "minecraft:player",
+                    ...(teamScore !== -1 ? {
+                        scoreOptions: [{
+                            objective: "team",
+                            minScore: teamScore,
+                            maxScore: teamScore,
+                            exclude: true
+                        }]
+                    } : {})
+                });
+
+                for (const target of targets) {
+                    target.applyDamage(10);
+                }
+
+                sword.remove();
+            }
+
+            prevY = currentY;
+        }, 1);
+
+        this.onCooldown(player);
+    }
+}
